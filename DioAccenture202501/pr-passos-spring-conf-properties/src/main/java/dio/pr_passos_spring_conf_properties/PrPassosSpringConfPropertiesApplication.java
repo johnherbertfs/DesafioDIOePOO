f9465971923ff1242899eaf85ee3028df5f22317
John H <johnherbertfs@gmail.com>
2026-03-14 01:07:48 -0300

    Teste pois adicionei mais projeto estudo sprintboot dentro da worktree git\DioAccenture20251 entao deve aparecer +1 pasta.

---
+++ DioAccenture202501/pr-passos-spring-conf-properties/src/main/java/dio/pr_passos_spring_conf_properties/PrPassosSpringConfPropertiesApplication.java
@@ -0,0 +1,16 @@
+package dio.pr_passos_spring_conf_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrPassosSpringConfPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrPassosSpringConfPropertiesApplication.class, args);
+		
+		
+		
+	}
+
+}
---
+++ DioAccenture202501/pr-passos-spring-conf-properties/src/test/java/dio/pr_passos_spring_conf_properties/PrPassosSpringConfPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.pr_passos_spring_conf_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrPassosSpringConfPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
