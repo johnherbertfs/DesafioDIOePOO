88431db2f56360d5b7c9a0b4e241d7dbb2c03c2c
John H <johnherbertfs@gmail.com>
2026-03-14 22:52:24 -0300

    Na interface repository foi adicionado um metodo com @query para buscar por parte do nome no banco e mostrar em tela.  ficar para futuro criar outros metodos.

--- DioAccenture202501/dio-pr-passos-spring-data-jpa/src/main/java/dio/dio_pr_passos_spring_data_jpa/app/StartApp.java
+++ DioAccenture202501/dio-pr-passos-spring-data-jpa/src/main/java/dio/dio_pr_passos_spring_data_jpa/app/StartApp.java
@@ -1,5 +1,7 @@
 package dio.dio_pr_passos_spring_data_jpa.app;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,8 +15,10 @@ import dio.dio_pr_passos_spring_data_jpa.repository.UserRepository;
 public class StartApp implements CommandLineRunner { // sera nosso main
 	
 	
+	
 	@Autowired
 	private UserRepository repository;
+	
 	
 	
 
@@ -24,18 +28,26 @@ public class StartApp implements CommandLineRunner { // sera nosso main
 		
 		System.out.println("|||||||||||||||||||||||||||||  AULA PRIMEIROS PASSOS SPRING: SPRING DATA JPA  |||||||||||||||||||||||||||||");
 		
-		UserApp userApp = new UserApp("John Herbert", "senha", "john.herbert");
+		UserApp userApp = new UserApp("Johnathan Herbert", "senha23", "nathan.herbert");
 		
 		// salvar usuario
 		repository.save(userApp);
 		
 		
 		// recuperar todos os usuarios salvos
+		System.out.println("Mostrando todos os Users cadastrados no banco:");
 		for (UserApp user_tmp : repository.findAll()) {
-			
 			System.out.println(user_tmp.toString());
-			
 		}
+		
+		
+		
+		System.out.println("Mostrando todos os Users que tem no nome 'Joao': ");
+		List<UserApp> usersComJoao = repository.filtrarPorNome("Joao");
+		for (UserApp user_tmp : usersComJoao) {
+			System.out.println(user_tmp.toString());
+		}
+		
 
 	}
 
--- DioAccenture202501/dio-pr-passos-spring-data-jpa/src/main/java/dio/dio_pr_passos_spring_data_jpa/repository/UserRepository.java
+++ DioAccenture202501/dio-pr-passos-spring-data-jpa/src/main/java/dio/dio_pr_passos_spring_data_jpa/repository/UserRepository.java
@@ -1,6 +1,10 @@
 package dio.dio_pr_passos_spring_data_jpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import dio.dio_pr_passos_spring_data_jpa.model.UserApp;
 
@@ -9,7 +13,8 @@ import dio.dio_pr_passos_spring_data_jpa.model.UserApp;
 public interface UserRepository extends JpaRepository<UserApp, Integer> {
 	
 	
-	
+	@Query("SELECT u FROM UserApp u WHERE u.name LIKE %:name%")
+	List<UserApp> filtrarPorNome(@Param("name") String name);
 	
 
 	
