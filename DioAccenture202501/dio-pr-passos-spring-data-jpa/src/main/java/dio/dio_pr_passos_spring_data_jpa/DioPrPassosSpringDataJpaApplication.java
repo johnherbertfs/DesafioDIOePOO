8bdc2a0b1b4a934679dc5e30844de40833651e67
John H <johnherbertfs@gmail.com>
2026-03-14 21:52:34 -0300

    Segundo commit complementar sobre salvar classe/objeto entidade e recuperar os dados.

---
+++ DioAccenture202501/dio-pr-passos-spring-data-jpa/src/main/java/dio/dio_pr_passos_spring_data_jpa/DioPrPassosSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package dio.dio_pr_passos_spring_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPrPassosSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPrPassosSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ DioAccenture202501/dio-pr-passos-spring-data-jpa/src/test/java/dio/dio_pr_passos_spring_data_jpa/DioPrPassosSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.dio_pr_passos_spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioPrPassosSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
