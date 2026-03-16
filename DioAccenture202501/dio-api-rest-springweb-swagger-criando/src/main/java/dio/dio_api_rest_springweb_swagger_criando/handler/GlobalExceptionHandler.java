c32eccf8e7cc1a9599138949afa4782d1050eff9
John H <johnherbertfs@gmail.com>
2026-03-15 21:54:01 -0300

    Adicionando depedencia swagger para documentacao automatica api via web.

---
+++ DioAccenture202501/dio-api-rest-springweb-swagger-criando/src/main/java/dio/dio_api_rest_springweb_swagger_criando/handler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package dio.dio_api_rest_springweb_swagger_criando.handler;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+
+
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
