a7b3bcf67c1ef670b9a3c896b90e54fedfd0c773
John H <johnherbertfs@gmail.com>
2026-04-11 04:14:46 -0300

    segundo commit complementar.

---
+++ DioBootcampAlmaviva2026/Almaviva-Springboot-Revisao2/springboot2-pr-passos/src/test/java/dio/springboot2_pr_passos/Springboot2PrPassosApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springboot2_pr_passos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2PrPassosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
